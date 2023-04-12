0a4e53edc6269cb048b7683eddc333ebaa7f7bd9
Dima <11006851+DimaGalitskov@users.noreply.github.com>
2023-04-12 11:42:19 +0100

    Initial commit

---
+++ CameraHandler.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SOULS
+{
+    public class CameraHandler : MonoBehaviour
+    {
+        public Transform targetTransform;
+        public Transform cameraTransform;
+        public Transform cameraPivotTransform;
+        private Transform myTransform;
+        private Vector3 cameraTransformPosition;
+        private LayerMask ignoreLayers;
+
+
+    }
+}
